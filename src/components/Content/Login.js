a8705057ceeccd721a6a5d887ae4bb99cd556549
frenchie048 <37281870+frenchie048@users.noreply.github.com>
2019-02-04 11:01:39 -0700

    finished, without redux

--- src/components/Content/Login.js
+++ src/components/Content/Login.js
@@ -8,13 +8,25 @@ export default class Login extends Component {
     };
   }
 
+  //can do this way, or can do inline (below)
+  // handleChange() {
+  //   this.setState({
+  //     name: e.target.value
+  //   })
+  // }
+
   render() {
     return (
       <div>
         <h1>Login!</h1>
         {/* save the onchange value to localstate and the submitted will value will get passed back to the parent */}
-        <input />
-        <button>Submit</button>
+        <input
+          value={this.state.name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+        />
+        <button
+          onClick={() => this.props.login(this.state.name)}>
+          Submit</button>
       </div>
     );
   }
